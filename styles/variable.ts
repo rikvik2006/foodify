4a696760f31bd1919000d254f211b862fd01e857
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2025-02-28 13:12:39 +0100

    feat: add stylesVariables for color management

---
+++ styles/variable.ts
@@ -0,0 +1,49 @@
+export const stylesVariables = {
+	colors: {
+		// Used for primary actions
+		primary: {
+			light: "#E1F4F3",
+			normal: "#00A58E",
+			dark: "#007A69",
+			darker: "#004D41",
+		},
+		// Used for backgrounds and borders
+		cloud: {
+			light: "#F5F7F9",
+			normal: "#E8EDF1",
+			dark: "#BAC7D5",
+		},
+		// Used for typography and icons
+		ink: {
+			light: "#BAC7D5",
+			normal: "#4F5E71",
+			dark: "#252A31",
+		},
+		// Used for comunicate success
+		success: {
+			light: "#EAF5EA",
+			normal: "#0A932B",
+			dark: "#2D7738",
+		},
+		// Used for comunicate warning
+		warning: {
+			light: "#FEF2E6",
+			normal: "#CF6C00",
+			dark: "#A75400",
+		},
+		// Used for comunicate errors
+		error: {
+			light: "#FAEAEA",
+			normal: "#D21C1C",
+			dark: "#970C0C",
+		},
+		focus: {
+			light: "#E8F4FD",
+			normal: "#0070C8",
+			dark: "#005AA3",
+		},
+		// Used for backgrounds of the content
+		white: "#FFFFFF",
+	},
+	radiusXl: 12,
+};
