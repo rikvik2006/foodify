7f037ae5c4bde427f19a174c05692a18f017edf6
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2025-03-01 09:09:58 +0100

    feat: implement custom fonts and enhance index page styling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function RootLayout() {
-  return <Stack />;
+	const [fontsLoaded, error] = useFonts({
+		LibreBaskervilleBold: require("@/assets/fonts/LibreBaskerville-Bold.ttf"),
+		Geist: require("@/assets/fonts/Geist-VariableFont_wght.ttf"),
+	});
+
+	useEffect(() => {
+		if (fontsLoaded || error) {
+			SplashScreen.hideAsync();
+		}
+	}, [fontsLoaded]);
+
+	if (!fontsLoaded && !error) {
+		return null;
+	}
+
+	return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,31 @@
-import { Text } from "react-native";
+import { Text, StyleSheet } from "react-native";
 import PageWrapper from "@/components/pageWrapper";
+import { stylesVariables } from "@/styles/variable";
 
 export default function Index() {
 	return (
-		<PageWrapper>
-			<Text>Home</Text>
+		<PageWrapper style={styles.container}>
+			<Text style={styles.welcome}>Hi &#123;USER&#125;</Text>
+			<Text style={styles.welcomeMessage}>
+				Today is an amazing day to track your calories
+			</Text>
 		</PageWrapper>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: stylesVariables.colors.cloud.light,
+	},
+	welcome: {
+		fontSize: 48,
+		fontWeight: 700,
+		marginTop: 100,
+		fontFamily: "LibreBaskervilleBold",
+	},
+	welcomeMessage: {
+		fontSize: 16,
+		marginTop: 5,
+		width: 252,
+	},
+});
