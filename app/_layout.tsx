cb65ae3770bcd9bd87936711e3214ee0abd27770
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2025-05-12 09:24:07 +0200

    chore: integrate Convex into the application

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,11 +4,17 @@ import { StatusBar } from "expo-status-bar";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
 import * as SplashScreen from "expo-splash-screen";
+// Convex
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 
 export default function RootLayout() {
 	const [fontsLoaded, error] = useFonts({
 		LibreBaskervilleBold: require("@/assets/fonts/LibreBaskerville-Bold.ttf"),
 		Geist: require("@/assets/fonts/Geist-VariableFont_wght.ttf"),
+	});
+
+	const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+		unsavedChangesWarning: false,
 	});
 
 	useEffect(() => {
@@ -22,11 +28,13 @@ export default function RootLayout() {
 	}
 
 	return (
-		<GestureHandlerRootView>
-			<Stack screenOptions={{ headerShown: false }}>
-				<Stack.Screen name="(tabs)" />
-			</Stack>
-			<StatusBar style="auto" />
-		</GestureHandlerRootView>
+		<ConvexProvider client={convex}>
+			<GestureHandlerRootView>
+				<Stack screenOptions={{ headerShown: false }}>
+					<Stack.Screen name="(tabs)" />
+				</Stack>
+				<StatusBar style="auto" />
+			</GestureHandlerRootView>
+		</ConvexProvider>
 	);
 }
